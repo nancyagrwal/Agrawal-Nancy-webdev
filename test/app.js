cf42c4ea246ff7d10242deef58d41a97f8340536
Nancy Agrawal <nancyagrwal@gmail.com>
2017-05-10 00:20:44 -0400

    Update app.js

--- test/app.js
+++ test/app.js
@@ -4,17 +4,17 @@ module.exports = function(app)
     app.post("/api/test", createMessage);
     app.delete("/api/test/:id", deleteMessage);
 
- //   var connectionString = 'mongodb://127.0.0.1:27017/test';
+    var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-   // if(process.env.MLAB_USERNAME) {
-      //  connectionString = process.env.MLAB_USERNAME + ":" +
-     //      process.env.MLAB_PASSWORD + "@" +
-        //    process.env.MLAB_HOST + ':' +
-         //   process.env.MLAB_PORT + '/' +
-          //  process.env.MLAB_APP_NAME;
-    //}
+    if(process.env.MLAB_USERNAME) {
+       connectionString = process.env.MLAB_USERNAME + ":" +
+           process.env.MLAB_PASSWORD + "@" +
+         process.env.MLAB_HOST + ':' +
+        process.env.MLAB_PORT + '/' +
+      process.env.MLAB_APP_NAME;
+    }
 
-    var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+ 
 if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
 	var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
 	var password = process.env.MLAB_PASSWORD_WEBDEV;
