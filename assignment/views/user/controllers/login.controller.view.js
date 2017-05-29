a8e5e234996b585f4380df1f649d1ea3ee9c748e
Nancy Agrawal <nancyagrwal@gmail.com>
2017-05-28 21:11:39 -0400

    client
    client

--- assignment/views/user/controllers/login.controller.view.js
+++ assignment/views/user/controllers/login.controller.view.js
@@ -6,8 +6,8 @@
     function loginController($location, userService) {
 
         var model = this;
-
         model.login = login;
+        model.register = register();
 
         function login(username, password) {
             var found = userService.findUserByCredentials(username, password);
@@ -17,6 +17,10 @@
             } else {
                 model.message = "sorry, " + username + " not found. please try again!";
             }
+        }
+
+        function register() {
+            $location.url("/register")
         }
     }
 })();
