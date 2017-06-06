9a9914cbdb9b34a5fb173dc5c1f4305afdfa651a
Nancy Agrawal <nancyagrwal@gmail.com>
2017-06-06 12:08:18 -0400

    app.js

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -1,11 +1,4 @@
 
-
-(function () {
-    angular
-        .module('WebAppMaker', ['ngRoute']);
-})();
-
-
 module.exports = function(app) {
     require("./services/ServerSide Services/user.service.server.js")(app);
     require("./services/ServerSide Services/website.service.server.js")(app);
