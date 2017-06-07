feb10d5b5a2863d3aa7d6846f555f65bd23756ab
Nancy Agrawal <nancyagrwal@gmail.com>
2017-06-07 13:46:17 -0400

    project 3

---
+++ public/project/services/user.service.client.js
@@ -0,0 +1,83 @@
+(function(){
+    angular
+        .module('Travelator')
+        .factory('userService', userService);
+    
+    function userService($http) {
+        /*var users = [
+            {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder",   email: "alice@wonder.com"  },
+            {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley",   email: "bob@marley.com"  },
+            {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia",   email: "charly@garcia.com"  },
+            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi",   email: "jose@annunzi.com" }
+        ];*/
+
+        var api = {
+            createUser: createUser,
+            findUserById: findUserById,
+            findUserByUsername: findUserByUsername,
+            findUserByCredentials: findUserByCredentials,
+            updateUser: updateUser,
+            deleteUser: deleteUser
+        };
+        return api;
+
+        function findUserById(userId) {
+            for(var u in users) {
+                if(users[u]._id === userId)
+                    return users[u];
+            }
+            return null;
+        }
+
+        function findUserByUsername(username) {
+            var url = "/api/assignment/user?username=" + username;
+            return $http.get(url)
+                .then(function (response) {
+                    return response.data;
+                });
+        }
+
+        function findUserByCredentials(username, password) {
+            var url = "/api/assignment/user?username=" + username + '&password=' + password;
+            return $http.get(url)
+                .then(function (response) {
+                    console.log(response.data);
+                    return response.data;
+                });
+        }
+
+
+
+        function findUserById(userId) {
+            var url = "/api/assignment/user/" + userId;
+            return $http.get(url)
+                .then(function (response) {
+                    return response.data;
+                });
+        }
+
+        function createUser(user) {
+            var url = "/api/assignment/user";
+            return $http.post(url, user)
+                .then(function (response) {
+                    return response.data;
+                });
+        }
+
+        function updateUser(userId, user) {
+            var url = "/api/assignment/user/" + userId;
+            return $http.put(url, user)
+                .then(function (response) {
+                    return response.data;
+                });
+        }
+
+        function deleteUser(userId) {
+            var url = "/api/assignment/user/" + userId;
+            return $http.delete(url)
+                .then(function (response) {
+                    return response.data;
+                });
+        }
+    }
+})();
