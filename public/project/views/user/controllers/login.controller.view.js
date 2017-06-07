3379486be2ef63f54cab2b1b099f38730e9f6af1
Nancy Agrawal <nancyagrwal@gmail.com>
2017-06-07 13:06:21 -0400

    project 3

---
+++ public/project/views/user/controllers/login.controller.view.js
@@ -0,0 +1,35 @@
+(function () {
+    angular
+        .module('WebAppMaker')
+        .controller('loginController', loginController);
+    
+    function loginController($location, userService) {
+
+        var model = this;
+
+        model.login = login;
+
+        function login(username, password) {
+
+            if(username === null || username === '' || typeof username === 'undefined') {
+                model.error = 'Please enter Username!';
+                return;
+            }
+
+            if(password === null || password === '' || typeof password === 'undefined') {
+                model.error = "Please enter Passwords!";
+                return;
+            }
+
+            userService
+                .findUserByCredentials(username, password)
+                .then(function (found) {
+                    if(found !== null) {
+                        $location.url('/user/' + found._id);
+                    } else {
+                        model.message = "sorry, " + username + " not found. please try again!";
+                    }
+                });
+        }
+    }
+})();
