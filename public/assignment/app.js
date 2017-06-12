f0ac483a0c87bfdd0fdd8a101e874706553e362c
Nancy Agrawal <nancyagrwal@gmail.com>
2017-06-12 19:42:14 -0400

    assignment 5

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -1,7 +1,7 @@
 
 (function () {
     angular
-        .module('WebAppMaker',['ngRoute' , 'textAngular']);
+        .module('WebAppMaker',['ngRoute' , 'textAngular' , 'wbdvDirectives']);
 
 }) ();
 
