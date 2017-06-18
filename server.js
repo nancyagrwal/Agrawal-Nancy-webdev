4d135704de840a418ac5898d97dee93908782c2b
Nancy Agrawal <nancyagrwal@gmail.com>
2017-06-18 00:57:17 -0400

    assignment 6

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ app.use(bodyParser.json());//For parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(app.express.static(__dirname + '/public'));
-//require ("./test/app.js")(app);
+require ("./test/app.js")(app);
 require('./assignment/app')(app);
-//require('./project/app');
+require('./project/app');
 var port = process.env.PORT || 3000;
 app.listen(port);
